17b20828f26fa4a3d1e3c003642816e8d2b133d7
chun <k236179@gmail.com>
2022-04-22 16:16:57 +0800

    新增user

--- user-form.php
+++ user-form.php
@@ -38,31 +38,13 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
                 <input type="text" class="form-control" name="password" id="inpit1" placeholder="輸入">
                 <label for="inpit1" class="form-label">gender</label>
                 <input type="text" class="form-control" name="gender" id="inpit1" placeholder="輸入">
-            </div>
-            <div class="mb-2">
-                     <label for="inpit1">birthday</label>
-                    <div class="row">
-                         <div class="col">
-                         <input type="text" id="startY"
-                         class="form-control" name="startY" placeholder="YYYY" required
-                         >年
-                         </div>
-                         <div class="col">
-                         <input type="text" id="startY"
-                         class="form-control" name="startM" placeholder="MM" required
-                         >月
-                         </div>
-                         <div class="col">
-                         <input type="text" id="startY"
-                         class="form-control" name="startD" placeholder="DD" required
-                         >日
-                    </div>
-                <div class="mb-3">
-                    <label for="inpit1" class="form-label">phone</label></label>
-                    <input type="text" class="form-control" name="phone" id="inpit1" placeholder="輸入">
-                    <label for="inpit1" class="form-label">photo</label></label>
-                    <input type="text" class="form-control" name="photo" id="inpit1" placeholder="輸入">
-                </div>            
+                <label for="inpit1" class="form-label">birthday</label>
+                <input type="date" class="form-control" name="birthday" id="inpit1" placeholder="輸入">
+                <label for="inpit1" class="form-label">phone</label></label>
+                <input type="text" class="form-control" name="phone" id="inpit1" placeholder="輸入">
+                <label for="inpit1" class="form-label">photo</label></label>
+                 <input type="text" class="form-control" name="photo" id="inpit1" placeholder="輸入">
+            </div>            
 
             <!-- 送出按鈕 -->
             <div class="text-center">
